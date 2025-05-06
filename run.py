253724c16d0f8d92143c6e436f9a70d59e7712d7
Mahdi <99555712+DevMti@users.noreply.github.com>
2025-05-06 22:43:23 +0330

    added a loggging info

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-import uvicorn
+import uvicorn, logging
 from config import WEB_SERVER_HOST, WEB_SERVER_PORT, WORKERS_COUNT
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s:     %(message)s')
+    logging.info(f"Starting server at {WEB_SERVER_HOST}:{WEB_SERVER_PORT} with {WORKERS_COUNT} workers")
     uvicorn.run(
         "bot.main:app",
         host=WEB_SERVER_HOST,
